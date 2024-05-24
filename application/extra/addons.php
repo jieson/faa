b749f42dc61e05ac016c6076080e09687c5512b8
孟杰 <317572877@qq.com>
2024-05-25 06:40:00 +0800

    安装 插件，会员邀请和二维码生成

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,8 +2,22 @@
 
 return [
     'autoload' => false,
-    'hooks' => [],
-    'route' => [],
+    'hooks' => [
+        'user_sidenav_after' => [
+            'invite',
+        ],
+        'user_register_successed' => [
+            'invite',
+        ],
+        'app_init' => [
+            'qrcode',
+        ],
+    ],
+    'route' => [
+        '/invite/[:id]$' => 'invite/index/index',
+        '/qrcode$' => 'qrcode/index/index',
+        '/qrcode/build$' => 'qrcode/index/build',
+    ],
     'priority' => [],
     'domain' => '',
 ];
