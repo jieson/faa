ed1b44cc3043d1b2cce77d9a565fde186405c56e
孟杰 <317572877@qq.com>
2024-05-24 19:57:19 +0800

    登录 鉴权，数据限制归属，  控制器 和 view传值

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -18,6 +18,23 @@ class Test extends Backend
      */
     protected $model = null;
 
+    // mj add
+    protected $noNeedLogin = ['work1']; //无需登录的方法,同时也就不需要鉴权了
+    protected $noNeedRight = ['test1']; //无需鉴权的方法,但需要登录
+
+    /**
+     * 是否开启数据限制
+     * 支持auth/personal
+     * 表示按权限判断/仅限个人
+     * 默认为禁用,若启用请务必保证表中存在admin_id字段
+     */
+    protected $dataLimit = 'auth';
+
+    /**
+     * 数据限制字段
+     */
+    protected $dataLimitField = 'admin_id';
+
     public function _initialize()
     {
         parent::_initialize();
@@ -28,6 +45,17 @@ class Test extends Backend
         $this->view->assign("hobbydataList", $this->model->getHobbydataList());
         $this->view->assign("statusList", $this->model->getStatusList());
         $this->view->assign("stateList", $this->model->getStateList());
+
+
+        //渲染站点配置
+//        $this->assign('site', $site);
+//        //渲染配置信息
+//        $this->assign('config', $config);
+//        //渲染权限对象
+//        $this->assign('auth', $this->auth);
+//        //渲染管理员对象
+//        $this->assign('admin', Session::get('admin'));
+        $this->assign('ceshi123', '测试123');
     }
 
 
@@ -38,5 +66,20 @@ class Test extends Backend
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
+    /**
+     * work1
+     */
+    public function work1()
+    {
+        return '无需登录和鉴权 work1';
+    }
+
+    /**
+     * test1
+     */
+    public function test1()
+    {
+        return '需要登录，不需要鉴权 tstt1';
+    }
 
 }
