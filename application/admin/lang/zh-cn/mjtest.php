71eeaa18bd428f58c4d78d76f17ff8c84d1335ce
孟杰 <317572877@qq.com>
2024-05-25 19:32:22 +0800

    一键生成mj_test  curd

---
+++ application/admin/lang/zh-cn/mjtest.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'User_id'     => '拥有者ID',
+    'Leader_id'   => '领导ID',
+    'User_name'   => '拥有者姓名',
+    'Leader_name' => '领导姓名',
+    'Title'       => '标题'
+];
---
+++ public/assets/js/backend/mjtest.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'mjtest/index' + location.search,
+                    add_url: 'mjtest/add',
+                    edit_url: 'mjtest/edit',
+                    del_url: 'mjtest/del',
+                    multi_url: 'mjtest/multi',
+                    import_url: 'mjtest/import',
+                    table: 'mjtest',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'user_id', title: __('User_id')},
+                        {field: 'leader_id', title: __('Leader_id')},
+                        {field: 'user_name', title: __('User_name'), operate: 'LIKE'},
+                        {field: 'leader_name', title: __('Leader_name'), operate: 'LIKE'},
+                        {field: 'title', title: __('Title'), operate: 'LIKE'},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
