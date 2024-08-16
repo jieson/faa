4a0c6fba0a6fa6195c1ce0f33ea2e92342944b2d
孟杰 <317572877@qq.com>
2024-08-16 17:10:50 +0800

    代表队显示 微调

--- public/assets/js/backend/ygame/team.js
+++ public/assets/js/backend/ygame/team.js
@@ -26,20 +26,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 fixedRightNumber: 1,
                 columns: [
                     [
-                        {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'user_id', title: __('User_id')},
-                        {field: 'project_id', title: __('Project_id')},
+                        // {checkbox: true},
                         {field: 'team_name', title: __('Team_name'), operate: 'LIKE'},
-                        {field: 'leader', title: __('Leader'), operate: 'LIKE'},
-                        {field: 'mobile', title: __('Mobile'), operate: 'LIKE'},
-                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
-                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
-                        {field: 'trainer_id', title: __('Trainer_id')},
-                        {field: 'admin_id', title: __('Admin_id')},
-                        {field: 'project.project_name', title: __('Project.project_name'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
-                        {field: 'user.nickname', title: __('User.nickname'), operate: 'LIKE'},
-                        {field: 'admin.nickname', title: __('Admin.nickname'), operate: 'LIKE'},
                         {
                             field: 'buttons',
                             width: "120px",
@@ -85,6 +73,20 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                             ],
                             formatter: Table.api.formatter.buttons
                         },
+                        {field: 'id', title: __('Id')},
+                        {field: 'user_id', title: __('User_id')},
+                        {field: 'project_id', title: __('Project_id')},
+
+                        {field: 'leader', title: __('Leader'), operate: 'LIKE'},
+                        {field: 'mobile', title: __('Mobile'), operate: 'LIKE'},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        {field: 'trainer_id', title: __('Trainer_id')},
+                        {field: 'admin_id', title: __('Admin_id')},
+                        {field: 'project.project_name', title: __('Project.project_name'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'user.nickname', title: __('User.nickname'), operate: 'LIKE'},
+                        {field: 'admin.nickname', title: __('Admin.nickname'), operate: 'LIKE'},
+
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
